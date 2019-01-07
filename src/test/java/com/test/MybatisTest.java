8b3a1fa10b2d3a8e36235f2c0cafa5f7c4bda102
GordenGong <296596331@qq.com>
2019-01-08 07:19:15 +0800

    初始化

---
+++ src/test/java/com/test/MybatisTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.umbrella.MySpringBootApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MySpringBootApplication.class)
+public class MybatisTest {
+}
